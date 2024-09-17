20164c17017ba30ba5aa0af8626b504b9ef025f5
Ibrahim Ahmed <ibrahimahmed0@outlook.com>
2024-09-17 20:39:42 +0100

    First Commit

---
+++ index.js
@@ -0,0 +1,58 @@
+// template_yybfzol
+// service_9rcnbdg
+// wLsQljjo_DJbRAHvb
+
+let infoToggle = false;
+function toggleInfo() {
+  if (infoToggle) {
+    infoToggle = false;
+    return document.body.classList.remove("about__slider");
+  }
+  infoToggle = true;
+  document.body.classList += " about__slider";
+}
+
+let contrastToggle = false;
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += " dark-theme";
+  } else {
+    document.body.classList.remove("dark-theme");
+  }
+}
+
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(" .about__overlay--loading");
+  const success = document.querySelector(" .about__overlay--success");
+  loading.classList += " about__overlay--visible";
+
+  emailjs
+    .sendForm(
+      "service_9rcnbdg",
+      "template_yybfzol",
+      event.target,
+      "wLsQljjo_DJbRAHvb"
+    )
+    .then(() => {
+      loading.classList.remove("about__overlay--visible");
+      success.classList += " about__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("about__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable, Please contact me directly on ibrahimahmed0@outlook.com"
+      );
+    });
+}
+
+let isAboutOpen = false;
+function toggleAbout() {
+  if (isAboutOpen) {
+    isAboutOpen = false;
+    return document.body.classList.remove("about--open");
+  }
+  isAboutOpen = true;
+  document.body.classList += " about--open";
+}
